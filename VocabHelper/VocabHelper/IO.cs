5ea22bb62abc7d879435da4fac0e6d833f3f9381
Alexander Wiencken <alwi58400@edu.ucl.dk>
2023-11-10 14:29:58 +0100

    - Created C# project - Added simple Grid making code - Created start of IO class with one method (ReadFile)

---
+++ VocabHelper/VocabHelper/IO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VocabHelper
+{
+    public class IO
+    {
+        public static void ReadFile(string path)
+        {
+            if (File.Exists(path))
+            {
+                using StreamReader sr = File.OpenText(path);
+                
+                while (!sr.EndOfStream)
+                { Debug.WriteLine(sr.ReadLine()); }
+            }
+        }
+    }
+}
---
+++ VocabHelper/VocabHelper/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualBasic;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VocabHelper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new()
+            {
+                InitialDirectory = "C:\\",
+                Filter = "text file (*.txt)|*.txt|csv file(*.csv)|*.csv|Any file (*.*)|*.*",
+                FilterIndex = 2,
+                RestoreDirectory = true
+            };
+
+            if (ofd.ShowDialog() == true)
+            { IO.ReadFile(ofd.FileName); }
+
+            // Ask for grid size
+            int size = int.Parse(Interaction.InputBox("Enter the size of the grid", "Set grid size", "15"));
+
+            // Create grid
+            Random r = new();
+            Grid g = new()
+            { ShowGridLines = true };
+
+            for (int i = 0; i < size; i++)
+            {
+                g.ColumnDefinitions.Add(new() { Width = new(1, GridUnitType.Star)});
+                g.RowDefinitions.Add(new() { Height = new(1, GridUnitType.Star) });
+
+                for (int j = 0; j < size; j++)
+                {
+                    TextBlock block = new()
+                    {
+                        HorizontalAlignment = HorizontalAlignment.Center,
+                        VerticalAlignment = VerticalAlignment.Center,
+                        Text = Convert.ToChar(r.Next(65, 91)).ToString()
+                    };
+
+                    Grid.SetColumn(block, j);
+                    Grid.SetRow(block, i);
+                    g.Children.Add(block);
+                }
+            }
+
+            this.Content = g;
+        }
+    }
+}
