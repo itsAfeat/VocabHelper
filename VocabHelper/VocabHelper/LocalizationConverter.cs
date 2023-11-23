ea9490519100c66d3ed45abbffc86925f29f250b
fUNNY fnaf <itsafeat@gmail.com>
2023-11-23 17:51:20 +0100

    Sikke noget lort

---
+++ VocabHelper/VocabHelper/LocalizationConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace VocabHelper
+{
+    [ValueConversion(typeof(string), typeof(string))]
+    public class LocalizationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameters, CultureInfo culture)
+        {
+            string paramStr = (string)parameters;
+            if (!string.IsNullOrEmpty(paramStr))
+            {
+                string[] paramArr = paramStr.Split('|');
+                return "";
+            }
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return "";
+        }
+    }
+}
